d0cf3a4437cf0e1477e36206fb7b64f5733fa702
laurentaubin <44675437+laurentaubin@users.noreply.github.com>
2022-02-20 18:49:32 -0500

    [Fix] Navbar Profile button had no feedback when on user profile (#96)

--- frontend/src/main/layout/navbar/NavBar.tsx
+++ frontend/src/main/layout/navbar/NavBar.tsx
@@ -1,13 +1,13 @@
 import { useRouter } from "next/router";
-import { BiBell, BiHome, BiMessageDetail, BiUser, BiSearchAlt } from "react-icons/bi";
+import { BiBell, BiHome, BiMessageDetail, BiSearchAlt, BiUser } from "react-icons/bi";
+import { LoginButton } from "../../../authentication/components/login/LoginButton";
+import { LogoutButton } from "../../../authentication/components/logout/LogoutButton";
+import { useAuth } from "../../hooks/useAuth";
 import { NavItem } from "./NavItem";
 import { NewPostButton } from "./NewPostButton";
-import { LogoutButton } from "../../../authentication/components/logout/LogoutButton";
-import { LoginButton } from "../../../authentication/components/login/LoginButton";
-import { useAuth } from "../../hooks/useAuth";
 
 export const NavBar = () => {
-  const { pathname } = useRouter();
+  const { pathname, query } = useRouter();
   const { isLoggedIn, me } = useAuth();
 
   const navItems: NavItem[] = [
@@ -38,15 +38,20 @@ export const NavBar = () => {
     }
   ];
 
+  const getPathName = (title: string): string => {
+    return title === "Profile" ? `/${query.username}` : pathname;
+  };
+
   return (
     <div className="md:px-8 md:sticky md:top-0">
       <div className="bg-white px-6 py-2 md:py-4 mx-auto">
         <div className="-mx-4 w-full flex flex-row md:block">
           {isLoggedIn ? (
             <>
-              {navItems.map((navItem) => (
-                <NavItem key={navItem.title} item={navItem} isActive={pathname === navItem.href} />
-              ))}
+              {navItems.map((navItem) => {
+                const pathName = getPathName(navItem.title);
+                return <NavItem key={navItem.title} item={navItem} isActive={pathName === navItem.href} />;
+              })}
 
               <LogoutButton />
               <NewPostButton />
